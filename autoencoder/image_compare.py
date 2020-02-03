237b77d97afb75159d560661314ec505bf8fd5ab
“NPU-Franklin <1109797240@qq.com>
2020-02-03 15:38:34 +0800

    add image compare part

--- autoencoder/image_compare.py
+++ autoencoder/image_compare.py
@@ -1,6 +1,6 @@
 """
-Autoencoder
-Model compile and train.
+Comparer
+Compare two of the images.
 
 Licensed under the GPL-3.0 License (see LICENSE for details)
 Written by NPU-Franklin@Github.com
@@ -12,20 +12,20 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mping
+from keras import backend as K
 
 from tensorflow import keras
 from keras.preprocessing.image import load_img, img_to_array
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Conv2D, AveragePooling2D, UpSampling2D
 from keras.callbacks import TensorBoard
-from tensorflow.python.keras.utils import plot_model
 
 # Requires TensorFlow 2.0.0 + and Keras 2.2.4+.
 from distutils.version import LooseVersion
 if LooseVersion(tf.__version__) >= LooseVersion("2.0.0"):
     print("WARNING: Your tensorflow version might be a little bit low to run it!")
 if LooseVersion(keras.__version__) >= LooseVersion("2.2.4"):
-    print("WARNING: Your keras version might be a little bit low to run it!") 
+    print("WARNING: Your keras version might be a little bit low to run it!")
 
 # Set font size and figure size.
 plt.rcParams['font.size'] = 18
@@ -33,6 +33,9 @@ plt.rcParams['figure.figsize'] = (18, 24)
 
 # Set CUDA Visual Device.
 os.environ['CUDA_VISIBLE_DEVICES'] = '/gpu:0'
+
+# Set numpy print threshold.
+np.set_printoptions(threshold=1000000)
 
 # Set size of the image.
 img_width, img_height, channels = 1536, 2048, 3
@@ -80,49 +83,33 @@ def load_data(dir):
     return arr
 
 # Load data.
-train_data = load_data(r'./datasets/304x304')
-print(train_data.shape)
+test_data = load_data(r'./datasets/304x304')
+print(test_data.shape)
 
-# Save data array as numpy array.
-if os.path.exists(r"./data_numpyarray"):
-    np.save('./data_numpyarray/dataset_304xx304_reshaped.npy', train_data)
+# Load trained model.
+if os.path.exists('./results/model/CAE_304x304_reshaped.h5'):
+    model = load_model('./results/model/CAE_304x304_reshaped.h5')
 else:
-    np.save('../data_numpyarray/dataset_304xx304_reshaped.npy', train_data)
+    model = load_model('../results/model/CAE_304x304_reshaped.h5')
 
-# Define model
-model = Sequential()
+# Get encoder part and its outputs.
+encoder = K.function([model.layers[0].input],model.layers[7].output)
+encoder_output = encoder([test_data])
+encoder_output = encoder_output.reshape(len(encoder_output), 49152)
 
-# encoder: （Conv2D + AveragePooling2D)*4
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape, padding='same'))
-model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-model.add(Conv2D(8, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-model.add(Conv2D(4, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
+# Calculate R_entropy
+def R_entropy(f, s):
+    KL = 0
+    for i, j in zip(encoder_output[f,:], encoder_output[s,:]):
+        if any(i == 0):
+            continue
+        else:
+            KL += i * np.log(i / (1 / 2 * i + 1 / 2 * j))
+    print("characteristic value: %.7f"%KL)
 
-# decoder: (UpSampling2D + Conv2D)*4 + conv2D
-model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(4, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(8, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(Conv2D(3, kernel_size=(3, 3), activation='sigmoid', padding='same'))
+print("Please input the first image you want to compare: ")
+f = int(input())
+print("Please input the second image you want to compare:")
+s = int(input())
 
-# Compile
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-model.summary()
-
-# Train
-model.fit(train_data, train_data, epochs=100, shuffle=True)
-
-# Save model
-if os.path.exists(r"./results"):
-    model.save('./results/model/CAE_304x304_reshaped.h5')
-else:
-    model.save('../results/model/CAE_304x304_reshaped.h5')
+R_entropy(f, s)
